be40743e9435a7e38a67ee37b880a457cdaa9500
Gazdek17 <33003055+Gazdek17@users.noreply.github.com>
2018-02-14 19:27:02 +0100

    ;)

---
+++ app/src/main/java/pl/lodz/uni/math/web_service/download_data.java
@@ -0,0 +1,67 @@
+package pl.lodz.uni.math.web_service;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Piotrek on 2018-02-14.
+ */
+
+public class download_data extends AsyncTask<Void,Void,Void> {
+    String data = "";
+    String dataParsed="";
+    String singleParsed="";
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://api.myjson.com/bins/l81ul");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while(line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+            JSONArray JA = new JSONArray(data);
+
+            for (int i=0; i<JA.length() ; i++){
+                JSONObject JO = (JSONObject) JA.get(i);
+                singleParsed = "name"+ JO.get("name")+ "\n"+
+                               "password"+ JO.get("password")+ "\n"+
+                               "contack"+ JO.get("contack")+ "\n"+
+                               "country"+ JO.get("country")+ "\n";
+
+                dataParsed = dataParsed + singleParsed + "\n";
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        MainActivity.data.setText(this.dataParsed);
+    }
+}
+
